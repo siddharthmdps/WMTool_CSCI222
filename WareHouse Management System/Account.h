d7539fd11b2cd0a05efedbc92bc19b2df973a09d
dlmenslon <36403246+dlmenslon@users.noreply.github.com>
2018-02-17 22:10:43 +0800

    Update Account.h
    Added private : string acccStatus, string currentpassword , missing verifyUser() , changePassword(), LoginMenu() and some mutators and stuff

--- WareHouse Management System/Account.h
+++ WareHouse Management System/Account.h
@@ -9,17 +9,19 @@
 #include <cstdlib>
 #include <cstring>
 #include <cctype>
+#include <sstream>
 
 
 using namespace std;
 
 class Account{
 
-    public:
+   	 public:
 		
 	
 		int FailedAttemptCount;	 //Track Failed Login
 		bool AccountStatus;	 //Track login
+		
 
 		std::string username;
 		std::string password;
@@ -28,16 +30,33 @@ class Account{
 		Account();	        
 		~Account();
 
+		void setUserLocation(int UL);
+		int  getUserLocation();
+
+		void setaccStatus(string AC);
+		string getaccStatus();
+
+		void setcurrentpassword(string CP);
+		string getcurrentpassword();
+
 		void Register();
-		void Login();		//File is delimited by ;
-	  	void Logout();
+		void LoginMenu();
+		void Login();			//File is delimited by ;
+	  	void Logout();	
 	  	void Stock();	
 	  	void Authentication();
 	  	void LockAccount();
 	  	void addWarningMessage();
 	  	void displayWarningMessage();
 	  	void ManageUser();
-	  
+		void changePassword();
+		void verifyUser();		//Called when user needs to be verified while doing certain tasks
+		
+	
+	private:
+	  	std::string currentpassword;	//For Authentication purposes
+		int UserLocation;	 	//Track which user is using program
+		std::string accStatus;
 	  	
 };
 
